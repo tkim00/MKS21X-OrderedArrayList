ba84f1895c6409ee40c04be6e85309f5bf5a6198
Taejoon Kim <tkim00@stuy.edu>
2018-11-03 22:45:31 -0400

    OrderedArrayList only one error away from finishing

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -7,16 +7,19 @@ public class NoNullArrayList<T> extends ArrayList<T>{
     super(startingCapacity);
   }
   public T set(int index, T element){
+    T x = super.get(index);
     if(element == null){
       throw new IllegalArgumentException("element cannot be null");
     }
     super.set(index, element);
+    return x;
   }
   public boolean add(T element){
     if(element == null){
       throw new IllegalArgumentException("element cannot be null");
     }
     super.add(element);
+    return true;
   }
   public void add(int index, T element){
     if(element == null){
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,12 +7,24 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
   public T set(int index, T element){
-    super.set(index, element);
+    T x = super.get(index);
+    super.remove(index);
+    add(element);
+    return x;
   }
   public void add(int index, T element){
-
+    super.add(findCorrectIndex(element), element);
+  }
+  public boolean add(T element){
+    super.add(findCorrectIndex(element), element);
+    return true;
   }
   private int findCorrectIndex(T element){
-    
+    for(int i = 0; i < size(); i++){
+      if(element.compareTo(super.get(i)) < 0){
+        return i;
+      }
+    }
+    return size();
   }
 }
